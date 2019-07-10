ee4f57152c8208a50b484ba6ef663138e9446d47
Eduardo Perez <edujpp1@gmail.com>
2019-07-10 13:20:47 -0300

    Se implementó el metodo handleChange que actualiza el state con la información que va ingresando el usuario. Esto se logro de una manera general que utilizándose según se ve en los comentario puede funcionar para otros formularios

--- src/components/NuevaCita.js
+++ src/components/NuevaCita.js
@@ -1,7 +1,41 @@
 import React, {Component} from 'react';
 
 class NuevaCita extends Component {
-  state = {}
+  state = {
+    cita:{
+      mascota: '',
+      propietario: '',
+      fecha: '',
+      hora: '',
+      sintomas: ''
+    }
+  }
+
+  handleChange = e => {
+    /*
+      Para que este metodo funcione en otros forms el name de cada
+      elemento del form debe coincidir con los atributos del state
+    */
+    // ver en cosole desde cuál input se está llamando al método
+    console.log(e.target.name+': '+e.target.value)
+
+    // colocar lo que el usuario escribe en el state
+    this.setState({
+      cita:{
+        /* 
+          Esta sintaxis permitira que sea mas dinamica la forma en
+          como se actualiza el state. e.target toma segun el name de
+          la etiqueta.
+          
+          Se recomienda tomar una copia del state antes de comenzar a
+          modificarlo para que no se pierdan esos datos, esto con el 
+          spread operator (...)
+        */
+       ...this.state.cita, // De esta forma los datos previos del state no se borran
+        [e.target.name]: e.target.value
+      }
+    })
+  }
 
   render(){
     return(
@@ -21,6 +55,8 @@ class NuevaCita extends Component {
                   className="form-control"
                   placeholder="Nombre Mascota"
                   name="mascota"
+                  onChange={this.handleChange}
+                  value={this.state.cita.mascota}
                 />
               </div>
             </div> {/* form-group*/}
@@ -35,6 +71,8 @@ class NuevaCita extends Component {
                   className="form-control"
                   placeholder="Nombre Dueño Mascota"
                   name="propietario"
+                  onChange={this.handleChange}
+                  value={this.state.cita.propietario}
                 />
               </div>
             </div> {/* form-group*/}
@@ -48,6 +86,8 @@ class NuevaCita extends Component {
                   type="date"
                   className="form-control"
                   name="fecha"
+                  onChange={this.handleChange}
+                  value={this.state.cita.fecha}
                 />
               </div>
               <label className="col-sm-4 col-lg-2 col-form-label">
@@ -58,6 +98,8 @@ class NuevaCita extends Component {
                   type="time"
                   className="form-control"
                   name="hora"
+                  onChange={this.handleChange}
+                  value={this.state.cita.hora}
                 />
               </div>
             </div> {/* form-group*/}
@@ -71,6 +113,8 @@ class NuevaCita extends Component {
                   className="form-control"
                   name="sintomas"
                   placeholder="Describe los síntomas"
+                  onChange={this.handleChange}
+                  value={this.state.cita.sintomas}
                 ></textarea>
               </div>
             </div> {/* form-group*/}
